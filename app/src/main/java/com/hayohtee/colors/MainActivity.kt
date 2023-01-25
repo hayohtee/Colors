70b64000c57c9cc81747e6d9cd5a106b9ef332b2
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-01-25 16:29:32 +0100

    Add a displayColor() method that get called when user tap the button to MainActivity.kt

--- app/src/main/java/com/hayohtee/colors/MainActivity.kt
+++ app/src/main/java/com/hayohtee/colors/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.hayohtee.colors
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.hayohtee.colors.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,35 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.createColorButton.setOnClickListener {
+            displayColor()
+        }
+    }
+
+    /**
+     * Extract the hex value from all the color channels if they are not empty and
+     * concatenate them to create a full RGB hex color value then sets the
+     * colorDisplayTextView to use the new color as the background color. Otherwise
+     * display a Toast to inform the user that all the fields should be filled
+     */
+    private fun displayColor() {
+        if (binding.redEditText.text?.isNotEmpty()!! &&
+            binding.greenEditText.text?.isNotEmpty()!! &&
+            binding.blueEditText.text?.isNotEmpty()!!
+        ) {
+
+            val redHexValue = binding.redEditText.text.toString()
+            val greenHexValue = binding.greenEditText.text.toString()
+            val blueValue = binding.blueEditText.text.toString()
+
+            val hexColor = "#$redHexValue$greenHexValue$blueValue"
+            binding.colorDisplayTextView.setBackgroundColor(Color.parseColor(hexColor))
+        } else {
+            Toast.makeText(
+                this, "Please enter required value for all the color channels",
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 }
